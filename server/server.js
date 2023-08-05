47125ff7cd48f059b1634236cfe8667bc283ce2e
tomaszsliwakowski <tomek08082002@gmail.com>
2023-08-05 11:29:34 +0200

    Changes to be committed: 	modified:   server/server.js

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const connectDB = require("./database/connectDB");
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "http://localhost:3000/E-Commers-Shop",
   credentials: true,
   optionSuccessStatus: 200,
 };
